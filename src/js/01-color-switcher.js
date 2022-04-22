7d3ad144a5f6156f41d3f4bd7cce8788a89de390
RomaSamkov <Romansamkov.rs@gmail.com>
2022-04-22 14:39:40 +0300

    add task 01-color-switcher and styles to button

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const refs = {
+    body: document.querySelector('body'),
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn: document.querySelector('[data-stop]'),
+};
 
+let timerId = null;
+
+refs.startBtn.addEventListener('click', onStartBtn);
+refs.stopBtn.addEventListener('click', onStopBtn);
+
+function onStartBtn() {
+    timerId = setInterval(changeColor, 1000);
+    refs.startBtn.setAttribute('disabled', '');
+};
+function onStopBtn() {
+    clearInterval(timerId);
+    refs.startBtn.removeAttribute('disabled');
+};
+
+function changeColor() {
+    refs.body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
